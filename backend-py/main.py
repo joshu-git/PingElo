8ef5d6a109604445f87903f5785cb5a60b12943d
joshu-git <argentjackjoshua@outlook.com>
2025-12-07 22:13:14 +0000

    Setup elo calculation with python
    Takes in game data and gives new elos

--- backend-py/main.py
+++ backend-py/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+#Data validation and parsing
+from pydantic import BaseModel
+
 from dotenv import load_dotenv
 import os
 
@@ -8,8 +12,6 @@ load_dotenv()
 
 #Create FastAPI app
 app = FastAPI()
-
-#Setup routes here
 
 #Enable CORS for the frontend URL
 allowed_origin = os.getenv("FRONTEND_URL")
@@ -26,3 +28,34 @@ app.add_middleware(
 @app.get("/")
 def root():
     return {"message": "Backend is running!"}
+
+#Elo calculation for every match
+
+#The data python expects from typescript
+class EloRequest(BaseModel):
+    RatingPlayerA: int
+    RatingPlayerB: int
+    ScorePlayerA: int
+    ScorePlayerB: int
+    GamePoints: int
+
+#The data python will send typescript
+class EloResponse(BaseModel):
+    NewRatingPlayerA: int
+    NewRatingPlayerB: int
+    Winner = str
+
+@app.post("/calculate_elo", response_model=EloResponse)
+def calculate_elo(request: EloRequest):
+    #Get previous elo of players
+    RatingA = request.RatingPlayerA
+    RatingB = request.RatingPlayerB
+
+    #Get score of each player
+    ScoreA = request.ScorePlayerA
+    ScoreB = request.ScorePlayerB
+
+    #Get how many points the game was
+    GamePoints = request.GamePoints
+
+    
