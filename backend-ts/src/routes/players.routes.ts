bd8d9f3c762956fb2b84cd51aea14109810e42aa
joshu-git <argentjackjoshua@outlook.com>
2025-12-22 21:27:58 +0000

    Update player routes for new database
    New error checks and variables

--- backend-ts/src/routes/players.routes.ts
+++ backend-ts/src/routes/players.routes.ts
@@ -1,73 +1,79 @@
 import { Router } from "express";
 
-import { supabase } from "../libs/supabase.js"
+import { supabase } from "../libs/supabase.js";
 
 //Authentication required for performing actions
-import { requireAuth, AuthenticatedRequest } from "../middleware/requireAuth.js";
+import {
+	AuthenticatedRequest,
+	requireAuth,
+} from "../middleware/requireAuth.js";
+import { requireAdmin } from "../middleware/requireAdmin.js";
 
 //Services required for routes
-import { createPlayer, claimPlayer } from "../services/players.services.js"
+import { createPlayer, claimPlayer } from "../services/players.services.js";
 
 const router = Router();
 
 //Route for creating a new player
-router.post("/create", requireAuth, async (req: AuthenticatedRequest, res) => {
-    try {
-        //Check if the user making the request is an admin
-        const { data: profile } = await supabase
-            .from("profiles")
-            .select("is_admin")
-            .eq("id", req.user!.id)
-            .single();
+router.post("/create", requireAdmin, async (req: AuthenticatedRequest, res) => {
+	try {
+		//Gets username from the request
+		const { player_name } = req.body;
 
-        //If the user is not an admin then throw an error
-        if (!profile?.is_admin) {
-            return res.status(403).json({ error: "Admin privileges required" });
-        }
+		//If there is no username an error is thrown
+		if (!player_name) {
+			return res.status(400).json({ error: "Player name required" });
+		}
 
-        //Gets username from the request
-        const { username } = req.body;
+		//createPlayer is called with the parameter username
+		const player = await createPlayer(player_name, req.group_id!);
 
-        //If there is no username an error is thrown
-        if (!username) {
-            return res.status(400).json({ error: "Player name required" });
-        }
-
-        //createPlayer is called with the parameter username
-        const player = await createPlayer(username);
-
-        //A success response is sent with the player's id, username and claim code
-        res.status(201).json({
-            message: "Player created successfully",
-            player
-        });
-    } catch (err: any) {
-        res.status(400).json({ error: err.message });
-    }
+		//A success response is sent with the player's id, username and claim code
+		res.status(201).json({
+			message: "Player created successfully",
+			player,
+		});
+	} catch (err: any) {
+		res.status(400).json({ error: err.message });
+	}
 });
 
 //Route for claiming a player
 router.post("/claim", requireAuth, async (req: AuthenticatedRequest, res) => {
-    try {
-        //Gets claimCode from the request
-        const { claimCode } = req.body;
+	try {
+		//Gets claimCode from the request
+		const { claim_code } = req.body;
 
-        //If there is no claimCode an error is thrown
-        if (!claimCode) {
-            return res.status(400).json({ error: "Claim code required" });
-        }
+		//If there is no claimCode an error is thrown
+		if (!claim_code) {
+			return res.status(400).json({ error: "Claim code required" });
+		}
 
-        //claimPlayer is called with parameters user.id and claimCode
-        const player = await claimPlayer(req.user!.id, claimCode);
+		//Check to see if the user already owns a player
+		const { data: existingPlayer } = await supabase
+			.from("players")
+			.select("id")
+			.eq("account_id", req.user!.id)
+			.single();
 
-        //A success response is sent with player's id and username
-        res.json({
-            message: 'Player claimed successfully',
-            player
-        });
-    } catch (err: any) {
-        res.status(400).json({ error: err.message });
-    }
+		//If they do own a player throw an error
+		if (existingPlayer) {
+			return res
+				.status(400)
+				.json({ error: "User already owns a player " });
+		}
+
+		//claimPlayer is called with parameters user.id and claimCode
+		const player = await claimPlayer(req.user!.id, claim_code);
+
+		//A success response is sent with player's id and username
+		res.json({
+			message: "Player claimed successfully",
+			player,
+		});
+	} catch (err: any) {
+		res.status(400).json({ error: err.message });
+	}
 });
 
 export default router;
