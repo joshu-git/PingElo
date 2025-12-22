f5fa5a7d62e857479dda7ced7f380ed4420b1e73
joshu-git <argentjackjoshua@outlook.com>
2025-12-22 21:27:13 +0000

    Update auth and admin checks
    Matches new columns in database

--- backend-ts/src/middleware/requireAdmin.ts
+++ backend-ts/src/middleware/requireAdmin.ts
@@ -2,31 +2,50 @@ import { Response, NextFunction } from "express";
 import { supabase } from "../libs/supabase.js";
 import { requireAuth, AuthenticatedRequest } from "./requireAuth.js";
 
-//Checks for authentication and admin
-export async function requireAdmin( req: AuthenticatedRequest, res: Response, next: NextFunction ) {
-  try {
-    //Ensures the user is authenticated
-    await new Promise<void>((resolve, reject) =>
-      requireAuth(req, res, (err?: any) => {
-        if (err) reject(err);
-        else resolve();
-      })
-    );
+//Checks for authentication, admin and to be in a group
+export async function requireAdmin(
+	req: AuthenticatedRequest,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		//Ensures the user is authenticated
+		await new Promise<void>((resolve, reject) =>
+			requireAuth(req, res, (err?: any) => {
+				if (err) reject(err);
+				else resolve();
+			})
+		);
 
-    //Checks the admin column
-    const { data, error } = await supabase
-      .from("profiles")
-      .select("is_admin")
-      .eq("id", req.user!.id)
-      .single();
+		//Checks if the user is an admin
+		const { data: admin, error: adminError } = await supabase
+			.from("account")
+			.select("is_admin")
+			.eq("id", req.user!.id)
+			.single();
 
-    if (error || !data?.is_admin) {
-      return res.status(403).json({ error: "Admin access required" });
-    }
+		if (adminError || !admin?.is_admin) {
+			return res.status(403).json({ error: "Admin required" });
+		}
 
-    //Move onto the real route handler
-    next();
-  } catch (err) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
+		//Checks if the user is in a group
+		const { data: group, error: groupError } = await supabase
+			.from("players")
+			.select("group_id")
+			.eq("id", req.user!.id)
+			.single();
+
+		//If the user is not an admin then throw an error
+		if (groupError || !admin?.is_admin) {
+			return res.status(403).json({ error: "Group required" });
+		}
+
+		//Allows groupId can be used in other functions
+		req.group_id = group.group_id;
+
+		//Move onto the real route handler
+		next();
+	} catch (err) {
+		return res.status(401).json({ error: "Unauthorized" });
+	}
 }
--- backend-ts/src/middleware/requireAuth.ts
+++ backend-ts/src/middleware/requireAuth.ts
@@ -1,38 +1,43 @@
 import { Request, Response, NextFunction } from "express";
-import { supabase } from "../libs/supabase.js"
+import { supabase } from "../libs/supabase.js";
 
 //Extends express request object to include user
 export interface AuthenticatedRequest extends Request {
-    user?: {
-        id: string;
-        email: string | undefined
-    };
+	user?: {
+		id: string;
+		email: string | undefined;
+	};
+	group_id?: string;
 }
 
 //Function for verifying a route request
-export async function requireAuth( req: AuthenticatedRequest, res: Response, next: NextFunction ) {
-    //Gets the frontend to send JWT token
-    const authHeader = req.headers.authorization;
+export async function requireAuth(
+	req: AuthenticatedRequest,
+	res: Response,
+	next: NextFunction
+) {
+	//Gets the frontend to send JWT token
+	const authHeader = req.headers.authorization;
 
-    //If there is no JWT token then throw an error
-    if (!authHeader) {
-        return res.status(401).json({ error: "Missing Authorization header" });
-    }
+	//If there is no JWT token then throw an error
+	if (!authHeader) {
+		return res.status(401).json({ error: "Missing Authorization header" });
+	}
 
-    //Extracts the actual JWT token
-    const token = authHeader.replace("Bearer ", "");
+	//Extracts the actual JWT token
+	const token = authHeader.replace("Bearer ", "");
 
-    //Validates the token with supabase
-    const { data, error } = await supabase.auth.getUser(token);
+	//Validates the token with supabase
+	const { data, error } = await supabase.auth.getUser(token);
 
-    //If the JWT is invalid or expired then block the request
-    if (error || !data.user) {
-        return res.status(401).json({ error: "Invalid or expired token" });
-    }
+	//If the JWT is invalid or expired then block the request
+	if (error || !data.user) {
+		return res.status(401).json({ error: "Invalid or expired token" });
+	}
 
-    //Attach user to the request so all routes can access id and email
-    req.user = { id: data.user.id, email: data.user.email ?? undefined };
+	//Attach user to the request so all routes can access id and email
+	req.user = { id: data.user.id, email: data.user.email ?? undefined };
 
-    //Move on to the real route handler
-    next();
+	//Move on to the real route handler
+	next();
 }
