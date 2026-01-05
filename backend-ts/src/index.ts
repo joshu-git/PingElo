e8d34fdcaea2cf5aa19781e97761051122e97b6e
joshu-git <argentjackjoshua@outlook.com>
2026-01-05 22:43:59 +0000

    Temporarily remove admin services and routes
    Allows the rest of the code to be debugged

--- backend-ts/src/index.ts
+++ backend-ts/src/index.ts
@@ -3,13 +3,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 //Gives access to player routes
-import playersRoutes from "./routes/players.routes.js"
+import playersRoutes from "./routes/players.routes.js";
 
 //Gives access to match routes
-import matchesRoutes from "./routes/matches.routes.js"
-
-//Gives access to admin routes
-import adminRoutes from  "./routes/admin.routes.js"
+import matchesRoutes from "./routes/matches.routes.js";
 
 //Load environment variables
 dotenv.config();
@@ -18,33 +15,34 @@ dotenv.config();
 const app = express();
 
 //Parses JSON bodies
-app.use(express.json())
+app.use(express.json());
 
 //Enable CORS for the frontend URL
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(
+	cors({
+		origin: process.env.FRONTEND_URL,
+		credentials: true,
+		allowedHeaders: ["Content-Type", "Authorization"],
+	})
+);
 
 //All routes for players, matches and admins
 app.use("/players", playersRoutes);
 app.use("/matches", matchesRoutes);
-app.use("/admin", adminRoutes)
 
 //Health check to confirm the backend is running
 app.get("/", (_req, res) => {
-    res.json({ message: "Backend is running" });
+	res.json({ message: "Backend is running" });
 });
 
 //Health check for UptimeRobot
 app.head("/health", (_req, res) => {
-  res.sendStatus(200);
+	res.sendStatus(200);
 });
 
 //Start server using Render Port or local dev
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`PingElo backend running on port ${PORT}`);
+	console.log(`PingElo backend running on port ${PORT}`);
 });
