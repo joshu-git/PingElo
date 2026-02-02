16e747f73d55a4cda7cf57a35fde149abb6e6e6d
joshu-git <argentjackjoshua@outlook.com>
2026-02-02 23:47:25 +0000

    Change Help button to Guide
    This is more professional for users

--- frontend/src/components/tournaments/Tournaments.tsx
+++ frontend/src/components/tournaments/Tournaments.tsx
@@ -109,7 +109,7 @@ export default function Tournaments() {
 					</Link>
 
 					<Link href="/documentation/tournaments">
-						<button className="px-4 py-2 rounded-lg">Help</button>
+						<button className="px-4 py-2 rounded-lg">Guide</button>
 					</Link>
 
 					<button
