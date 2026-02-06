e294f1bba785a11820a001eae2c8b8c17f398de5
joshu-git <argentjackjoshua@outlook.com>
2026-02-06 09:00:12 +0000

    Remove claim code logic
    It is no longer going to be used

--- backend-ts/src/routes/players.routes.ts
+++ backend-ts/src/routes/players.routes.ts
@@ -2,73 +2,37 @@ import { Router } from "express";
 
 import { supabase } from "../libs/supabase.js";
 
-//Authentication required for performing actions
 import {
 	AuthenticatedRequest,
 	requireAuth,
 } from "../middleware/requireAuth.js";
-import { requireAdmin } from "../middleware/requireAdmin.js";
 
-//Services required for routes
-import { createPlayer, claimPlayer } from "../services/players.services.js";
+import { createPlayer } from "../services/players.services.js";
 
 const router = Router();
 
 //Route for creating a new player
-router.post("/create", requireAdmin, async (req: AuthenticatedRequest, res) => {
+router.post("/create", requireAuth, async (req: AuthenticatedRequest, res) => {
 	try {
-		//Gets username from the request
 		const { player_name } = req.body;
 
-		//If there is no username an error is thrown
-		if (!player_name) {
+		if (!player_name)
 			return res.status(400).json({ error: "Player name required" });
-		}
 
-		//createPlayer is called with the parameter username
-		const player = await createPlayer(player_name, req.group_id!);
-
-		//A success response is sent with the player's id, username and claim code
-		res.status(201).json({
-			message: "Player created successfully",
-			player,
-		});
-	} catch (err: any) {
-		res.status(400).json({ error: err.message });
-	}
-});
-
-//Route for claiming a player
-router.post("/claim", requireAuth, async (req: AuthenticatedRequest, res) => {
-	try {
-		//Gets claimCode from the request
-		const { claim_code } = req.body;
-
-		//If there is no claimCode an error is thrown
-		if (!claim_code) {
-			return res.status(400).json({ error: "Claim code required" });
-		}
-
-		//Check to see if the user already owns a player
-		const { data: existingPlayer } = await supabase
+		const { data: duplicatePlayer } = await supabase
 			.from("players")
-			.select("id")
-			.eq("account_id", req.user!.id)
-			.single();
+			.select("account_id")
+			.eq("account_id", req.user!.id);
 
-		//If they do own a player throw an error
-		if (existingPlayer) {
+		if (duplicatePlayer)
 			return res
 				.status(400)
-				.json({ error: "User already owns a player " });
-		}
+				.json({ error: "Account already has a player" });
 
-		//claimPlayer is called with parameters user.id and claimCode
-		const player = await claimPlayer(req.user!.id, claim_code);
+		const player = await createPlayer(player_name, req.group_id!);
 
-		//A success response is sent with player's id and username
-		res.json({
-			message: "Player claimed successfully",
+		res.status(201).json({
+			message: "Player created successfully",
 			player,
 		});
 	} catch (err: any) {
--- backend-ts/src/services/players.services.ts
+++ backend-ts/src/services/players.services.ts
@@ -1,74 +1,24 @@
 //For using supabase calls
 import { supabase } from "../libs/supabase.js";
 
-import { PlayersRow } from "../types/database.js";
-
-//For generating claim codes
-import { v4 as uuidv4 } from "uuid";
-
-//Used for creating a claimable player
+//Used for creating a player
 export async function createPlayer(playerName: string, group_id: string) {
-	//Generate a unique claim code for the player
-	const claim_code = uuidv4().slice(0, 8).toUpperCase();
-
 	//Create the new player in players
 	const { data, error } = await supabase
 		.from("players")
 		.insert({
 			player_name: playerName,
-			claim_code: claim_code,
 			group_id: group_id,
 		})
 		.select()
 		.single();
 
-	//If there was an error or there is no data returned
 	if (error || !data) {
 		throw new Error(error?.message || "Failed to create player");
 	}
 
-	//Return the player's ID, username and claim code
 	return {
 		id: data.id,
 		player_name: data.player_name,
-		claim_code: data.claim_code,
-	};
-}
-
-//Used for claiming a player that has been created
-export async function claimPlayer(account_id: string, claim_code: string) {
-	//Find the unclaimed player
-	const { data: player, error: findError } = await supabase
-		.from("players")
-		.select("*")
-		.eq("claim_code", claim_code)
-		.is("account_id", null)
-		.single();
-
-	//If the code could not be found or the user_id was not null
-	if (findError || !player) {
-		throw new Error("Invalid or already used code");
-	}
-
-	//Update the user_id with their ID and make claim_code null
-	const { data: updatedPlayer, error: updateError } = await supabase
-		.from("players")
-		.update({
-			account_id: account_id,
-			claim_code: null,
-		})
-		.eq("id", player.id)
-		.select()
-		.single();
-
-	//If there is an error updating show message
-	if (updateError) {
-		throw new Error(updateError.message);
-	}
-
-	//Return the player's ID and username
-	return {
-		id: updatedPlayer.id,
-		player_name: updatedPlayer.player_name,
 	};
 }
