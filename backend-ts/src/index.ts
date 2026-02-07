029998b9c350ca4534c71d3f543bf5e612fcabc5
joshu-git <argentjackjoshua@outlook.com>
2026-02-07 12:16:30 +0000

    Fix groups routes as they were using matches
    Corrects the import for groupsRoutes

--- backend-ts/src/index.ts
+++ backend-ts/src/index.ts
@@ -2,28 +2,17 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-//Gives access to players routes
 import playersRoutes from "./routes/players.routes.js";
-
-//Gives access to matches routes
 import matchesRoutes from "./routes/matches.routes.js";
-
-//Gives access to groups routes
-import groupsRoutes from "./routes/matches.routes.js";
-
-//Gives access to groups routes
+import groupsRoutes from "./routes/groups.routes.js";
 import tournamentsRoutes from "./routes/tournaments.routes.js";
 
-//Load environment variables
 dotenv.config();
 
-//Create the Express app
 const app = express();
 
-//Parses JSON bodies
 app.use(express.json());
 
-//Enable CORS for the frontend URL
 app.use(
 	cors({
 		origin: process.env.FRONTEND_URL,
@@ -32,13 +21,12 @@ app.use(
 	})
 );
 
-//All routes for players, matches and admins
+//All routes
 app.use("/players", playersRoutes);
 app.use("/matches", matchesRoutes);
 app.use("/groups", groupsRoutes);
 app.use("/tournaments", tournamentsRoutes);
 
-//Health check to confirm the backend is running
 app.get("/", (_req, res) => {
 	res.json({ message: "Backend is running" });
 });
@@ -48,7 +36,6 @@ app.head("/health", (_req, res) => {
 	res.sendStatus(200);
 });
 
-//Start server using Render Port or local dev
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
