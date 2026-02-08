d9928635a15b91f167bf1043ae5b9b01590ff0ea
joshu-git <argentjackjoshua@outlook.com>
2026-02-08 19:54:53 +0000

    Clean up authentication code
    Makes the code more debuggable

--- backend-ts/src/middleware/requireAdmin.ts
+++ backend-ts/src/middleware/requireAdmin.ts
@@ -2,7 +2,7 @@ import { Response, NextFunction } from "express";
 import { supabase } from "../libs/supabase.js";
 import { requireAuth, AuthenticatedRequest } from "./requireAuth.js";
 
-//Checks for authentication, admin and to be in a group
+//Checks for authentication, admin and group
 export async function requireAdmin(
 	req: AuthenticatedRequest,
 	res: Response,
@@ -17,14 +17,12 @@ export async function requireAdmin(
 			})
 		);
 
-		//Checks if the user is an admin
 		const { data: admin, error: adminError } = await supabase
 			.from("account")
 			.select("is_admin")
 			.eq("id", req.user!.id)
 			.single();
 
-		//If the user is not an admin then throw an error
 		if (adminError || !admin?.is_admin) {
 			return res.status(403).json({ error: "Admin required" });
 		}
@@ -36,15 +34,13 @@ export async function requireAdmin(
 			.eq("account_id", req.user!.id)
 			.single();
 
-		//If the user is not in a group then throw an error
 		if (groupError || !group?.group_id) {
 			return res.status(403).json({ error: "Group required" });
 		}
 
-		//Allows groupId can be used in other functions
+		//Allows groupId to be used in other functions
 		req.group_id = group.group_id;
 
-		//Move onto the real route handler
 		next();
 	} catch (err) {
 		return res.status(401).json({ error: "Unauthorized" });
--- backend-ts/src/middleware/requireAuth.ts
+++ backend-ts/src/middleware/requireAuth.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { supabase } from "../libs/supabase.js";
 
-//Extends express request object to include user
 export interface AuthenticatedRequest extends Request {
 	user?: {
 		id: string;
@@ -16,21 +15,17 @@ export async function requireAuth(
 	res: Response,
 	next: NextFunction
 ) {
-	//Gets the frontend to send JWT token
 	const authHeader = req.headers.authorization;
 
-	//If there is no JWT token then throw an error
 	if (!authHeader) {
 		return res.status(401).json({ error: "Missing Authorization header" });
 	}
 
-	//Extracts the actual JWT token
 	const token = authHeader.replace("Bearer ", "");
 
 	//Validates the token with supabase
 	const { data, error } = await supabase.auth.getUser(token);
 
-	//If the JWT is invalid or expired then block the request
 	if (error || !data.user) {
 		return res.status(401).json({ error: "Invalid or expired token" });
 	}
@@ -38,6 +33,5 @@ export async function requireAuth(
 	//Attach user to the request so all routes can access id and email
 	req.user = { id: data.user.id, email: data.user.email ?? undefined };
 
-	//Move on to the real route handler
 	next();
 }
